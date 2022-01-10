488aef0eb2146c2dcfece7da4b9ea7b9dfdde695
AYH9601 <ays9499@naver.com>
2022-01-10 11:30:53 +0900

    time2 class change

--- js/main.js
+++ js/main.js
@@ -1,10 +1,32 @@
 ﻿const time = document.querySelectorAll(".screen span");
+const main = document.querySelector("main");
+const menus = document.querySelector("nav span");
 
 setInterval(()=>{
     let now = new Date();
     let hr = now.getHours();
     let min = now.getMinutes();
     let sec = now.getSeconds();
+
+    if(hr>=5 && hr<11){
+        main.className = "";
+        main.classList.add("morning")
+    }
+
+    if(hr>=11 && hr<16){
+        main.className = "";
+        main.classList.add("afternoon")
+    }
+
+    if(hr>=16 && hr<19){
+        main.className = "";
+        main.classList.add("evening")
+    }
+
+    if(hr>=19 || hr<5){
+        main.className = "";
+        main.classList.add("night")
+    }
 
     if(hr<10) hr= "0"+hr;
     if(min<10) min = "0"+min;
@@ -14,16 +36,3 @@ setInterval(()=>{
     time[1].innerText = min;
     time[2].innerText = sec;
 },1000)
-
-
-
-
-
-
-
-
-
-
-
-
-
